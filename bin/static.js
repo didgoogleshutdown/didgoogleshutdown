f4a4abf9588040ee0755021a124b639449c08bf3
OKNoah <noahgray@me.com>
2016-06-25 04:18:25 -0700

    Adds HTTPS/SSL support

--- bin/static.js
+++ bin/static.js
@@ -1,11 +1,37 @@
 var http = require('http'),
 	express = require('express'),
 	path = require('path'),
-	morgan = require('morgan')
+	morgan = require('morgan'),
+	enforce = require('express-sslify');
 
 var indexPath = path.resolve( __dirname, '../dist/client' )
 
 app = express();
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+app.get('/.well-known/acme-challenge/:acmeToken', function(req, res, next) {
+  var acmeToken = req.params.acmeToken;
+  var acmeKey;
+
+  if (process.env.ACME_KEY && process.env.ACME_TOKEN) {
+    if (acmeToken === process.env.ACME_TOKEN) {
+      acmeKey = process.env.ACME_KEY;
+    }
+  }
+
+  for (var key in process.env) {
+    if (key.startsWith('ACME_TOKEN_')) {
+      var num = key.split('ACME_TOKEN_')[1];
+      if (acmeToken === process.env['ACME_TOKEN_' + num]) {
+        acmeKey = process.env['ACME_KEY_' + num];
+      }
+    }
+  }
+
+  if (acmeKey) res.send(acmeKey);
+  else res.status(404).send();
+})
 
 app
 	.use( morgan( 'dev' ) )
